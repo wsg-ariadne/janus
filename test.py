21065e0e2080529790f377cab9fde31d249ce518
Juris Adorna <jurisadorna@japtop.local>
2023-05-12 13:24:03 +0800

    README: added usage and details on dataset, training

--- test.py
+++ test.py
@@ -14,15 +14,22 @@ import matplotlib.pyplot as plt
 # Reconstructing janus:
 reconstructed_janus = load_model("janus")
 
-# Declaring the path to the image:
-image_path = "final-dataset/train/weighted/AFPPopup.png"
+# Taking image path:
+image_path = input("Input image path: ")
 
-# Transforming the image into a standardized numpy array:
+# Transforming image to standardized numpy array:
 img_arr=cv2.imread(image_path)
 img_arr=cv2.resize(img_arr,(224,224))
 test_input=np.array([img_arr])
 test_input=test_input/225.0
 
-# Checking and printing the result:
-# 0 is absent, 1 is even, 2 is weighted
-print(reconstructed_janus.predict(test_input).argmax(axis=1)[0])
+# Checking the result:
+result = reconstructed_janus.predict(test_input).argmax(axis=1)[0]
+
+# Printing the result:
+if result == 0: 
+	print("Result for " + image_path + " is ABSENT.")
+if result == 1: 
+	print("Result for " + image_path + " is EVEN.")
+if result == 2: 
+	print("Result for " + image_path + " is WEIGHTED.")
