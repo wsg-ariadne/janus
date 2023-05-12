34d149b59501e9004e6e4f190c9d39657de4a845
Juris Adorna <jurisadorna@japtop.local>
2023-05-12 15:35:21 +0800

    generate.py: safeguarded against regenerating janus

--- generate.py
+++ generate.py
@@ -11,8 +11,141 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-# Reconstructing janus:
-reconstructed_janus = load_model("janus")
+# Image pre-processing:
+train_path="final-dataset/train"
+test_path="final-dataset/test"
+val_path="final-dataset/val"
+x_train=[]
+
+for folder in os.listdir(train_path):
+    sub_path=train_path+"/"+folder
+
+    for img in os.listdir(sub_path):
+        image_path=sub_path+"/"+img
+        img_arr=cv2.imread(image_path)
+        img_arr=cv2.resize(img_arr,(224,224))
+        x_train.append(img_arr)
+
+x_test=[]
+
+for folder in os.listdir(test_path):
+    sub_path=test_path+"/"+folder
+
+    for img in os.listdir(sub_path):
+        image_path=sub_path+"/"+img
+        img_arr=cv2.imread(image_path)
+        img_arr=cv2.resize(img_arr,(224,224))
+        x_test.append(img_arr)
+
+x_val=[]
+
+for folder in os.listdir(val_path):
+    sub_path=val_path+"/"+folder
+
+    for img in os.listdir(sub_path):
+        image_path=sub_path+"/"+img
+        img_arr=cv2.imread(image_path)
+        img_arr=cv2.resize(img_arr,(224,224))
+        x_val.append(img_arr)
+
+# Normalizing x_test, x_train, and x_val:
+train_x=np.array(x_train)
+test_x=np.array(x_test)
+val_x=np.array(x_val)
+train_x=train_x/255.0
+test_x=test_x/255.0
+val_x=val_x/255.0
+
+# Computing labels
+train_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+val_datagen = ImageDataGenerator(rescale = 1./255)
+training_set = train_datagen.flow_from_directory(train_path,
+                                                 target_size = (224, 224),
+                                                 batch_size = 32,
+                                                 class_mode = 'sparse')
+test_set = test_datagen.flow_from_directory(test_path,
+                                            target_size = (224, 224),
+                                            batch_size = 32,
+                                            class_mode = 'sparse')
+val_set = val_datagen.flow_from_directory(val_path,
+                                            target_size = (224, 224),
+                                            batch_size = 32,
+                                            class_mode = 'sparse')
+
+# Setting training, testing, and validation sets:
+train_y=training_set.classes
+test_y=test_set.classes
+val_y=val_set.classes
+
+# Model training:
+IMAGE_SIZE = [224, 224]
+vgg = VGG19(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+#do not train the pre-trained layers of VGG-19
+for layer in vgg.layers:
+    layer.trainable = False
+
+x = Flatten()(vgg.output)
+
+#adding output layer.Softmax classifier is used as it is multi-class classification
+prediction = Dense(3, activation='softmax')(x)
+model = Model(inputs=vgg.input, outputs=prediction)
+model.summary()
+
+# Compiling the model:
+model.compile(
+  loss='sparse_categorical_crossentropy',
+  optimizer="adam",
+  metrics=['accuracy']
+)
+
+# Ensuring that the model is not overfitting:
+from tensorflow.keras.callbacks import EarlyStopping
+early_stop=EarlyStopping(monitor='val_loss',mode='min',verbose=1,patience=5)
+
+history = model.fit(
+  train_x,
+  train_y,
+  validation_data=(val_x,val_y),
+  epochs=10,
+  callbacks=[early_stop],
+  batch_size=32,shuffle=True)
+
+# Visualizing performance (accuracies, loss):
+print("Performance graphs: ")
+plt.plot(history.history['accuracy'], label='train acc')
+plt.plot(history.history['val_accuracy'], label='val acc')
+plt.legend()
+plt.savefig('graphs/new_accgraph.png')
+plt.show()
+
+plt.plot(history.history['loss'], label='train loss')
+plt.plot(history.history['val_loss'], label='val loss')
+plt.legend()
+plt.savefig('graphs/new_lossgraph.png')
+plt.show()
+
+# Model evaluation
+model.evaluate(test_x,test_y,batch_size=32)
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+import numpy as np
+
+#predict
+y_pred=model.predict(test_x)
+y_pred=np.argmax(y_pred,axis=1)
+#get classification report
+print("Classification report: ")
+print(classification_report(y_pred,test_y))
+#get confusion matrix
+print("Confusion matrix: ")
+print(confusion_matrix(y_pred,test_y))
+
+# Saving the model:
+model.save('janus')
+
+# Reconstructing the saved model:
+reconstructed_janus = load_model("new_janus")
 
 # Declaring the path to the image:
 image_path = "final-dataset/train/weighted/AFPPopup.png"
